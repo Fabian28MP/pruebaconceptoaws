7688dc14d1bdb141a3a17ab8ed8741a170cfada1
Fabian Mautino <fabian_mautino@usmp.pe>
2024-08-13 09:39:23 -0500

    prueba para conexion con amplify

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
+import AppNavigator from './src/navigation/AppNavigator';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <AppNavigator />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +15,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
